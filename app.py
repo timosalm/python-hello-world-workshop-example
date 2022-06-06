8a549274bfa1a7e2cf2f10658b8a98a5a30a11e3
Timo Salm <tsalm@vmware.com>
2022-06-06 10:10:36 +0200

    first commit

--- app.py
+++ app.py
@@ -2,9 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
+def hello_world():
     return 'Hello from VMware Tanzu!'
-
-
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
